ba7ffceed19bf8c7b3b800e353cfd69dc82c2860
Sebastian-Kramer <sk265@hdm-stuttgart.de>
2017-05-05 15:25:29 +0200

    Die Klasse Projektmarktplatz wurde erweitert mit Attributen und Getter/Setter-Methoden

--- src/de/hdm/ITProjekt/shared/bo/Projektmarktplatz.java
+++ src/de/hdm/ITProjekt/shared/bo/Projektmarktplatz.java
@@ -1,5 +1,33 @@
 package de.hdm.ITProjekt.shared.bo;
 
 public class Projektmarktplatz {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private int ID = 0; 
+	private String bez = "";
+	
+	public Projektmarktplatz(){
+		
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public String getBez() {
+		return bez;
+	}
+
+	public void setBez(String bez) {
+		this.bez = bez;
+	}
+	
+	
+
 
 }
