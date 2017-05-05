5afebcc622236a01b1dbcfbf3529ec36938b5379
Sebastian-Kramer <sk265@hdm-stuttgart.de>
2017-05-05 14:52:03 +0200

    Datenbankeinbindung angelegt und localhost eingebunden (Test)

--- src/de/hdm/ITProjekt/server/db/DBConnection.java
+++ src/de/hdm/ITProjekt/server/db/DBConnection.java
@@ -1,5 +1,34 @@
 package de.hdm.ITProjekt.server.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+
+
 public class DBConnection {
+	
+	private static Connection con = null;
+
+	private static String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private static String localUrl = "jdbc:mysql://localhost:3306/IT Projekt";
+    private static String user = "root";
+    private static String password = "";
+    
+    public static Connection connection(){
+    	
+    	if(con == null){
+    		try {
+    			con = DriverManager.getConnection(localUrl, user, password);
+    			return con;
+    			}catch(Exception e){
+    				System.out.println(e);
+    			}
+    		
+    	}
+    	return con;
+    }
+    		
 
 }
