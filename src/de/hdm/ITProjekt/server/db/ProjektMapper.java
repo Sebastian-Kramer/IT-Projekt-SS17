42900440e56ce6e093e372f93c070aa17e8fe0b6
Sebastian-Kramer <sk265@hdm-stuttgart.de>
2017-05-09 18:38:16 +0200

    Änderung des ProjektMappers mit verschiedenen Methoden

--- src/de/hdm/ITProjekt/server/db/ProjektMapper.java
+++ src/de/hdm/ITProjekt/server/db/ProjektMapper.java
@@ -1,6 +1,8 @@
 package de.hdm.ITProjekt.server.db;
 
+
 import de.hdm.ITProjekt.shared.bo.Projekt;
+import de.hdm.ITProjekt.shared.bo.Projektmarktplatz;
 import de.hdm.ITProjekt.server.db.DBConnection;
 import java.sql.*;
 import java.util.Vector;
@@ -25,13 +27,16 @@ public class ProjektMapper {
 		
 		try{
 			Statement stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT ID, bez FROM Projekt "
-          + "WHERE ID=" + id + " ORDER BY bez");
+			ResultSet rs = stmt.executeQuery("SELECT ID, name, beschreibung, startdatum, enddatum FROM Projekt "
+          + "WHERE ID=" + id + " ORDER BY ID");
 			
 			if(rs.next()){
 				Projekt p = new Projekt();
 				p.setID(rs.getInt("ID"));
-				p.setName(rs.getString("bez"));
+				p.setName(rs.getString("name"));
+				p.setBeschreibung(rs.getString("beschreibung"));
+				p.setStartdatum(rs.getDate("startdatum"));
+				p.setEnddatum(rs.getDate("enddatum"));
 				return p;
 			}
 		}
@@ -40,6 +45,64 @@ public class ProjektMapper {
 			return null;
 		}
 		return null;	
+	}
+	
+	public Vector<Projekt> getAll(){
+		
+		 Connection con = DBConnection.connection();
+		 
+		 Vector<Projekt> result = new Vector<Projekt>();
+		 
+		  try {
+		      Statement stmt = con.createStatement();
+
+		      ResultSet rs = stmt.executeQuery("SELECT ID, name, beschreibung, startdatum, enddatum FROM Projektmarktplatz");
+		  
+		  while (rs.next()) {
+			  	Projekt p = new Projekt();
+			  	p.setID(rs.getInt("ID"));
+				p.setName(rs.getString("name"));
+				p.setBeschreibung(rs.getString("beschreibung"));
+				p.setStartdatum(rs.getDate("startdatum"));
+				p.setEnddatum(rs.getDate("enddatum"));
+			  
+			  result.addElement(p);
+		  }
+		}
+		    catch (SQLException e2) {
+		        e2.printStackTrace();
+		      }
+		  return result;
+	}
+	
+	public Projekt addProjekt(Projekt pmp){
+		
+		Connection con = DBConnection.connection();
+		
+		try {
+		      Statement stmt = con.createStatement();
+		      
+		      ResultSet rs = stmt.executeQuery("SELECT MAX(ID) AS maxid "
+		              + "FROM Projekt ");
+		      
+		
+		      if(rs.next()){
+		    	  
+		    	  	pmp.setID(rs.getInt("maxid") + 1);
+		    	  
+		    	  	stmt = con.createStatement();
+		    	  	
+		    		stmt.executeUpdate("INSERT INTO Projekt (ID , name, beschreibung, startdatum, enddatum)" 
+		    		+ "VALUES (" + pmp.getID() + "," + "'" + pmp.getName() + "'" + "," + "'" + pmp.getBeschreibung() 
+		    		+ "'" + pmp.getStartdatum()  +  pmp.getEnddatum() +")"); 
+		    	  
+		      }
+		}
+		catch(SQLException e2){
+			e2.printStackTrace();
+		}
+		return pmp;
+		
 	}
 }
 
