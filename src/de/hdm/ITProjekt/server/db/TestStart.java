af35775b890d798bed0f75ae2bf75ae28616d326
Sebastian-Kramer <sk265@hdm-stuttgart.de>
2017-05-09 18:51:52 +0200

    Anpassung der TestStart Klasse

--- src/de/hdm/ITProjekt/server/db/TestStart.java
+++ src/de/hdm/ITProjekt/server/db/TestStart.java
@@ -2,26 +2,46 @@ package de.hdm.ITProjekt.server.db;
 
 import de.hdm.ITProjekt.server.db.ProjektmarktplatzMapper;
 import de.hdm.ITProjekt.shared.bo.Projektmarktplatz;
+import de.hdm.ITProjekt.shared.bo.Projekt;
+import java.util.*;
+import java.text.*;
+
+import com.ibm.icu.text.SimpleDateFormat;
+
 
 public class TestStart {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
 		ProjektmarktplatzMapper t1 = ProjektmarktplatzMapper.pmpMapper();
+		ProjektMapper p = ProjektMapper.pMapper();
 		
 		Projektmarktplatz p1 = new Projektmarktplatz();
+		Projekt projekt =  new Projekt();
 		
-		//p1.setBez("Vertrieb");
+		
+		SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd");
+		Date startdatum = date.parse("2017-01-01");
+		Date enddatum = date.parse("2017-12-31");
+		
+		
+		/* Test Methodenaufrufe:
+		 	p1.setBez("Vertrieb");
+			t1.addMarktplatz(p1);		
+			p1.setID(2);
+			p1.setBez("Rechnungswesen");
+			t1.deleteMarktplatz(p1);
+		*/
 
-		//t1.addMarktplatz(p1);
+		projekt.setName("test");
+		projekt.setBeschreibung("test");
+		projekt.setStartdatum(startdatum);
+		projekt.setEnddatum(enddatum);
+
+	
+		p.addProjekt(projekt);
 		
-		p1.setID(2);
-		
-		//p1.setBez("Rechnungswesen");
-		
-		t1.deleteMarktplatz(p1);
-		
-		System.out.println(t1.getAll());
+		System.out.println(p.findByKey(2));
 	}
 
 }
