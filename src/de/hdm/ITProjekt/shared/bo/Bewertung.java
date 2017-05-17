c998bcbe063bf7e4ab36fdea8820ad077145ea58
RaphaelPantzakis <rp026@hdm-stuttgart.de>
2017-05-17 10:56:39 +0200

    KlasseBewertung

--- src/de/hdm/ITProjekt/shared/bo/Bewertung.java
+++ src/de/hdm/ITProjekt/shared/bo/Bewertung.java
@@ -1,5 +1,43 @@
 package de.hdm.ITProjekt.shared.bo;
+import java.util.Date;
 
-public class Bewertung {
+public class Bewertung extends BusinessObject {
+	private static final long serialVersionUID = 1L;
+	private String text;
+	private double wert;
+	private Date erstellungsdatum;
+	
+	public Bewertung(){
+	}
+	
+	public Bewertung(String text, double wert, Date erstellungsdatum){
+		this.text= text;
+		this.wert= wert;
+		this.erstellungsdatum= erstellungsdatum;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public double getWert() {
+		return wert;
+	}
+
+	public void setWert(double wert) {
+		this.wert = wert;
+	}
+
+	public Date getErstellungsdatum() {
+		return erstellungsdatum;
+	}
+
+	public void setErstellungsdatum(Date erstellungsdatum) {
+		this.erstellungsdatum = erstellungsdatum;
+	}
 
 }
