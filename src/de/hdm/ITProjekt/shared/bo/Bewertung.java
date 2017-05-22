c838ba3500cfaba7e45b2f6ee0f1808f1650412b
Sebastian-Kramer <sk265@hdm-stuttgart.de>
2017-05-22 15:54:28 +0200

    Klasse Bewertung mit allen Attributen und Methoden angepasst

--- src/de/hdm/ITProjekt/shared/bo/Bewertung.java
+++ src/de/hdm/ITProjekt/shared/bo/Bewertung.java
@@ -2,42 +2,93 @@ package de.hdm.ITProjekt.shared.bo;
 import java.util.Date;
 
 public class Bewertung extends BusinessObject {
+	
 	private static final long serialVersionUID = 1L;
-	private String text;
-	private double wert;
-	private Date erstellungsdatum;
 	
-	public Bewertung(){
+	private int ID = 0;
+	private String stellungnahme = "";
+	private double bewertung = 0;
+	//private Date erstellungsdatum;
+	private int Beteiligungs_ID = 0;
+	private int Bewerbungs_ID = 0;
+	
+	
+	
+	
+	public int getID() {
+		return ID;
+	}
+
+
+
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+
+
+
+	public String getStellungnahme() {
+		return stellungnahme;
+	}
+
+
+
+
+	public void setStellungnahme(String stellungnahme) {
+		this.stellungnahme = stellungnahme;
+	}
+
+
+
+
+	public double getBewertung() {
+		return bewertung;
+	}
+
+
+
+
+	public void setBewertung(double bewertung) {
+		this.bewertung = bewertung;
+	}
+
+
+
+
+	public int getBeteiligungs_ID() {
+		return Beteiligungs_ID;
+	}
+
+
+
+
+	public void setBeteiligungs_ID(int beteiligungs_ID) {
+		Beteiligungs_ID = beteiligungs_ID;
+	}
+
+
+
+
+	public int getBewerbungs_ID() {
+		return Bewerbungs_ID;
+	}
+
+
+
+
+	public void setBewerbungs_ID(int bewerbungs_ID) {
+		Bewerbungs_ID = bewerbungs_ID;
+	}
+
+
+
+
+	public String toString(){
+		return "\n" + "ID: " + ID + "\n" + "Stellungnahme: " + stellungnahme + "\n"
+				+ "Beurteilung: " + bewertung + "\n" + "Diese Beteiligung gehört zu folgender Bewerbung: " + Bewerbungs_ID
+				 + "\n";
 	}
 	
-	public Bewertung(String text, double wert, Date erstellungsdatum){
-		this.text= text;
-		this.wert= wert;
-		this.erstellungsdatum= erstellungsdatum;
-	}
-
-	public String getText() {
-		return text;
-	}
-
-	public void setText(String text) {
-		this.text = text;
-	}
-
-	public double getWert() {
-		return wert;
-	}
-
-	public void setWert(double wert) {
-		this.wert = wert;
-	}
-
-	public Date getErstellungsdatum() {
-		return erstellungsdatum;
-	}
-
-	public void setErstellungsdatum(Date erstellungsdatum) {
-		this.erstellungsdatum = erstellungsdatum;
-	}
-
 }
