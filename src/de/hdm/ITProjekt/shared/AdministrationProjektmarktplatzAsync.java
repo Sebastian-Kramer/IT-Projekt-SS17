d396d08001d4cbfb1aa3f7314bf96b2f6360bcbc
RaphaelPantzakis <rp026@hdm-stuttgart.de>
2017-05-23 12:50:54 +0200

    Async angelegt

---
+++ src/de/hdm/ITProjekt/shared/AdministrationProjektmarktplatzAsync.java
@@ -0,0 +1,25 @@
+package de.hdm.ITProjekt.shared;
+
+import java.util.Vector;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+import de.hdm.ITProjekt.shared.bo.Projektmarktplatz;
+
+public interface AdministrationProjektmarktplatzAsync {
+
+	void init(AsyncCallback<Void> callback);
+
+	void createProjektmarktplatz(String bezeichnung, AsyncCallback<Projektmarktplatz> callback);
+
+	void save(Projektmarktplatz p, AsyncCallback<Void> callback);
+
+	void getProjektmarktplatzAll(AsyncCallback<Vector<Projektmarktplatz>> callback);
+
+	void getProjektmarktplatzById(int ID, AsyncCallback<Projektmarktplatz> callback);
+
+	void deleteProjektmarktplatz(Projektmarktplatz p, AsyncCallback<Void> callback);
+
+	void updateProjektmarktplatz(Projektmarktplatz p, AsyncCallback<Void> callback);
+
+}
