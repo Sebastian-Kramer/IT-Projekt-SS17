862d9e8727ebea04c0b847c451e5dc4c92f71e2c
RaphaelPantzakis <rp026@hdm-stuttgart.de>
2017-05-23 12:50:37 +0200

    Verwaltungsklasse/interface AdministrationProjektmarktplatz erstellt
    Methoden für Projektmarktplatz angelegt

---
+++ src/de/hdm/ITProjekt/shared/AdministrationProjektmarktplatz.java
@@ -0,0 +1,35 @@
+package de.hdm.ITProjekt.shared;
+import java.util.ArrayList;
+import java.util.Vector;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+import de.hdm.ITProjekt.shared.bo.*;
+
+
+@RemoteServiceRelativePath("administrationProjektmarktplatz")
+public interface AdministrationProjektmarktplatz extends RemoteService {
+	
+	public void init() throws IllegalArgumentException; //Initialisierung des Objekts, muss nach Instantiierung aufgerufen werden
+	
+	public Projektmarktplatz createProjektmarktplatz(String bez)
+		throws IllegalArgumentException;
+	
+	public void save(Projektmarktplatz p) throws IllegalArgumentException; //speichern eines Projektmarktplatz-Objekts in der Datenbank
+	
+	public Projektmarktplatz getProjektmarktplatzById (int ID) throws IllegalArgumentException; //Anzeigen eines bekannten Projektmarktplatz
+	
+	public Vector<Projektmarktplatz> getProjektmarktplatzAll() throws IllegalArgumentException; //Anzeigen aller Projektmarktplätze
+	
+	public void deleteProjektmarktplatz(Projektmarktplatz p) throws IllegalArgumentException; //Löschen eines Projektmarktplatzes
+	
+	public void updateProjektmarktplatz(Projektmarktplatz p) throws IllegalArgumentException; //Editieren eines Projektmarktplatzes
+	
+	
+	
+	
+	
+	
+
+}
