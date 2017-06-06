0401e5344c1cd2888f7f57d791a6a165800ecc88
Sebastian-Kramer <sk265@hdm-stuttgart.de>
2017-06-06 17:41:31 +0200

    Klasse Person jetzt vollständig

--- src/de/hdm/ITProjekt/shared/bo/Person.java
+++ src/de/hdm/ITProjekt/shared/bo/Person.java
@@ -3,28 +3,20 @@ package de.hdm.ITProjekt.shared.bo;
 public class Person extends Organisationseinheit{
 	
 	private static final long serialVersionUID = 1L;
+	
 	private String vorname = "";
-	private String nachname = "";
+	
+	private String name = "";
+	
 	private String anrede = "";
-	private String anschrift = "";
-	private int ID = 0;
-	private int Orga_ID = 0; 
 	
-	public Person(){
-	}
-	public Person(String vorname, String nachname, String anrede){
-		this.vorname = vorname;
-		this.nachname = nachname;
-		this.anrede = anrede;
-	}
+	//Fremdschlüsselbeziehungen werden hier realisiert
 	
-	public int getID() {
-		return ID;
-	}
-
-	public void setID(int iD) {
-		ID = iD;
-	}
+	private Integer Team_ID = null; 
+	
+	private Integer UN_ID = null;
+	
+	
 	
 	
 	public String getVorname() {
@@ -33,11 +25,11 @@ public class Person extends Organisationseinheit{
 	public void setVorname(String vorname) {
 		this.vorname = vorname;
 	}
-	public String getNachname() {
-		return nachname;
+	public String getName() {
+		return name;
 	}
-	public void setNachname(String nachname) {
-		this.nachname = nachname;
+	public void setName(String nachname) {
+		this.name = nachname;
 	}
 	public String getAnrede() {
 		return anrede;
@@ -45,18 +37,29 @@ public class Person extends Organisationseinheit{
 	public void setAnrede(String anrede) {
 		this.anrede = anrede;
 	}
-	public String getAnschrift() {
-		return anschrift;
+	
+	public Integer getTeam_ID() {
+		return Team_ID;
 	}
-	public void setAnschrift(String anschrift) {
-		this.anschrift = anschrift;
+	
+	public void setTeam_ID(Integer team_ID) {
+		if(Team_ID==0){
+			this.Team_ID=null;
+		}else{
+			this.Team_ID = team_ID;	
+		}
 	}
-	public int getOrga_ID() {
-		return Orga_ID;
+	public Integer getUN_ID() {
+		return UN_ID;
 	}
-	public void setOrga_ID(int orga_ID) {
-		Orga_ID = orga_ID;
+	public void setUN_ID(Integer uN_ID) {
+		if(UN_ID==0){
+			this.UN_ID=null;
+		}else{
+			this.UN_ID = uN_ID;
+		}
 	}
+	
 
 	
 
