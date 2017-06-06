9fca3d03e24ed0b47f399670d501c59b3617dc20
Sebastian-Kramer <sk265@hdm-stuttgart.de>
2017-06-06 14:24:55 +0200

    Es können jetzt alle Ausschreibungen eines Projekts angezeigt werden

--- src/de/hdm/ITProjekt/server/db/AusschreibungMapper.java
+++ src/de/hdm/ITProjekt/server/db/AusschreibungMapper.java
@@ -67,7 +67,6 @@ public class AusschreibungMapper {
 				  while (rs.next()) {
 					Ausschreibung a = new Ausschreibung();
 					
-					a.setId(rs.getInt("Ausschreibung_Id"));
 				 	a.setID(rs.getInt("ID"));
 					a.setAusschreibungstext(rs.getString("ausschreibungstext"));
 					a.setBezeichnung(rs.getString("bezeichnung"));
