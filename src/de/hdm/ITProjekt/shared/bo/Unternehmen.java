ab13eba8e086a84f2e0abe8b35a782bdbb85727c
Sebastian-Kramer <sk265@hdm-stuttgart.de>
2017-06-06 15:31:25 +0200

    Anapssung der des UnternehmensMapper

--- src/de/hdm/ITProjekt/shared/bo/Unternehmen.java
+++ src/de/hdm/ITProjekt/shared/bo/Unternehmen.java
@@ -3,9 +3,9 @@ package de.hdm.ITProjekt.shared.bo;
 public class Unternehmen extends Organisationseinheit{
 	
 	private static final long serialVersionUID = 1L;
+	
 	private String name;
-	private String Firmensitz;
-	private int UN_ID = 0;
+
 	
 	public String getName() {
 		return name;
@@ -13,19 +13,5 @@ public class Unternehmen extends Organisationseinheit{
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getFirmensitz() {
-		return Firmensitz;
-	}
-	public void setFirmensitz(String firmensitz) {
-		Firmensitz = firmensitz;
-	}
-	public int getUN_ID() {
-		return UN_ID;
-	}
-	public void setUN_ID(int uN_ID) {
-		UN_ID = uN_ID;
-	}
-	
-	
-	
+
 }
