fd6c55e7e982ae1ba2fc4afb447eaca4fb1865b6
Sebastian-Kramer <sk265@hdm-stuttgart.de>
2017-06-06 16:29:13 +0200

    Mapperklasse Unternehmen angepasst

--- src/de/hdm/ITProjekt/server/db/UnternehmenMapper.java
+++ src/de/hdm/ITProjekt/server/db/UnternehmenMapper.java
@@ -8,13 +8,11 @@ import java.util.Vector;
 
 import de.hdm.ITProjekt.shared.bo.Organisationseinheit;
 import de.hdm.ITProjekt.shared.bo.Person;
-import de.hdm.ITProjekt.shared.bo.Projektmarktplatz;
+import de.hdm.ITProjekt.shared.bo.Team;
 import de.hdm.ITProjekt.shared.bo.Unternehmen;
 
-public class UnternehmenMapper {
+public class UnternehmenMapper extends OrganisationseinheitMapper{
 
-	
-	
 	
 	private static UnternehmenMapper unMapper = null;
 	
@@ -31,18 +29,23 @@ public class UnternehmenMapper {
 
 
 public Unternehmen findByKey(int id){
+	
 	Connection con = DBConnection.connection();
 	
 	try{
 		Statement stmt = con.createStatement();
-		ResultSet rs = stmt.executeQuery("SELECT ID, name, firmensitz FROM Unternehmen "
+		ResultSet rs = stmt.executeQuery("SELECT ID, name FROM Unternehmen "
       + "WHERE ID=" + id);
 		
 		if(rs.next()){
 			Unternehmen un = new Unternehmen();
 			un.setID(rs.getInt("ID"));
 			un.setName(rs.getString("name"));
-			un.setFirmensitz(rs.getString("firmensitz"));
+			un.setStraße(super.findByKey(id).getStraße());
+			un.setHausnummer(super.findByKey(id).getHausnummer());
+			un.setOrt(super.findByKey(id).getOrt());
+			un.setPlz(super.findByKey(id).getPlz());
+			un.setPartnerprofil_ID(super.findByKey(id).getPartnerprofil_ID());
 			return un;
 		}
 	}
@@ -54,7 +57,12 @@ public Unternehmen findByKey(int id){
 	
 }
 	
-	public Vector<Unternehmen> getAll(){
+	public Unternehmen findByObject(Unternehmen u){
+		return this.findByKey(u.getID());
+	}
+
+	
+	public Vector<Unternehmen> getAllUnternehmen(){
 		
 		 Connection con = DBConnection.connection();
 		 
@@ -66,21 +74,26 @@ public Unternehmen findByKey(int id){
 		      ResultSet rs = stmt.executeQuery("SELECT * FROM Unternehmen");
 		  
 		  while (rs.next()) {
-			  Unternehmen un = new Unternehmen();
-			  un.setID(rs.getInt("ID"));
-			  un.setName(rs.getString("name"));
-			  un.setFirmensitz(rs.getString("firmensitz"));
-			  
-			  result.addElement(un);
+			  Unternehmen u = new Unternehmen();
+				u.setID(rs.getInt("Unternehmen_Id"));
+				u.setName(rs.getString("Name"));
+				u.setStraße(super.findByObject(u).getStraße());
+				u.setHausnummer(super.findByObject(u).getHausnummer());
+				u.setPlz(super.findByObject(u).getPlz());
+				u.setOrt(super.findByObject(u).getOrt());
+				u.setPartnerprofil_ID(super.findByObject(u).getPartnerprofil_ID());
+			 
+				result.addElement(u);
 		  }
 		}
 		    catch (SQLException e2) {
 		        e2.printStackTrace();
+		        return null;
 		      }
 		  return result;
 	}
 	
-public void createUnternehmen(Unternehmen unternehmen1){
+public void createUnternehmen(Unternehmen u){
 		
 		Connection con = DBConnection.connection();
 		
@@ -93,16 +106,12 @@ public void createUnternehmen(Unternehmen unternehmen1){
 		
 		      if(rs.next()){
 		    	  
-		    	  	unternehmen1.setID(rs.getInt("maxid") + 1);
+		    	  	u.setID(rs.getInt("maxid") + 1);
 		    	  
 		    	  	stmt = con.createStatement();
 		    	  	
-		    		stmt.executeUpdate("INSERT INTO organisationseinheit (ID , name, firmensitz)" + "VALUES "
-		    				+ "("+  unternehmen1.getID()+ "," + "'" + unternehmen1.getName() + "'" + "," + "'" + 
-		    				unternehmen1.getFirmensitz()+ "'" +")");
-		    		
-		    		
-		    	  
+		    		stmt.executeUpdate("INSERT INTO organisationseinheit (ID , name, )" + "VALUES "
+		    				+ "("+  u.getID()+ "," + "'" + u.getName() + "'" +")");		    				    			    	  
 		      }
 		}
 		catch(SQLException e2){
@@ -114,21 +123,20 @@ public void createUnternehmen(Unternehmen unternehmen1){
 
 
 
-public Unternehmen updateUnternehmen(Unternehmen unternehmen1) {
+public Unternehmen updateUnternehmen(Unternehmen u) {
     Connection con = DBConnection.connection();
 
     try {
       Statement stmt = con.createStatement();
 
       stmt.executeUpdate("UPDATE organisationseinheit SET " + "Name=\""
-          + unternehmen1.getName() + "\", " + "firmensitz=\""
-          + unternehmen1.getFirmensitz() + "\" " + "WHERE id=" + unternehmen1.getID());
+          + u.getName() + "\"" + "WHERE ID=" + u.getID());
     }
     catch (SQLException e2) {
       e2.printStackTrace();
     }
 
-    return unternehmen1;
+    return u;
   }
 
 
