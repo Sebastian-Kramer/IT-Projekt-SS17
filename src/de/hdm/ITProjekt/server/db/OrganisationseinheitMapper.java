4a69c4cb701f907f11f84af1f24df864badd7cce
Sebastian-Kramer <sk265@hdm-stuttgart.de>
2017-06-06 16:23:03 +0200

    Klasse Orgaeinheit plus Mapper angepasst und vervollständigt

--- src/de/hdm/ITProjekt/server/db/OrganisationseinheitMapper.java
+++ src/de/hdm/ITProjekt/server/db/OrganisationseinheitMapper.java
@@ -34,14 +34,18 @@ public class OrganisationseinheitMapper {
 		
 		try{
 			Statement stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT ID, vorname, nachname, straße, hausnummer, plz, ort FROM Organisationseinheit "
+			ResultSet rs = stmt.executeQuery("SELECT ID, straße, hausnummer, plz, ort, Partnerprofil_ID FROM Organisationseinheit "
           + "WHERE ID=" + id);
 			
 			if(rs.next()){
 				Organisationseinheit o = new Organisationseinheit();
 				o.setID(rs.getInt("ID"));
-				o.setVorname(rs.getString("vorname"));
-				o.setNachname(rs.getString("nachname"));
+				o.setStraße(rs.getString("straße"));
+				o.setHausnummer(rs.getInt("hausnummer"));
+				o.setPlz(rs.getInt("plz"));
+				o.setOrt(rs.getString("ort"));
+				o.setPartnerprofil_ID(rs.getInt("Partnerprofil_ID"));
+				
 				return o;
 			}
 		}
@@ -50,102 +54,112 @@ public class OrganisationseinheitMapper {
 			return null;
 		}
 		return null;	
+	
+	}
+	protected Organisationseinheit findByObject(Organisationseinheit o){
+		return this.findByKey(o.getID()); 
 	}
 	
+	public Organisationseinheit findByForeignPartnerprofilId(int partnerprofilId){
+		Connection con = DBConnection.connection();
+			
+			try{
+				Statement stmt = con.createStatement();
+				ResultSet rs = stmt.executeQuery("SELECT ID, straße, hausnummer, plz, ort, Partnerprofil_ID FROM Organisationseinheit "
+						+ "WHERE Partnerprofil_ID=" + partnerprofilId);
+				
+				
+				if(rs.next()){
+					Organisationseinheit o = new Organisationseinheit();
+					o.setID(rs.getInt("ID"));
+					o.setStraße(rs.getString("straße"));
+					o.setHausnummer(rs.getInt("hausnummer"));
+					o.setPlz(rs.getInt("plz"));
+					o.setOrt(rs.getString("ort"));
+					o.setPartnerprofil_ID(rs.getInt("Partnerprofil_ID"));
+			return o;
+				
+				}
+		}
+			catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+			return null;
+		}
 	
-	public Vector<Projektmarktplatz> getAll(){
+	
+		public int insert(Organisationseinheit o){
+		 
+	    Connection con = DBConnection.connection();
+	    int id=0;
+	    try {
+	    		
+	      Statement stmt = con.createStatement();
+
+	      /*
+	       * Zunächst schauen wir nach, welches der momentan höchste
+	       * Primärschlüsselwert ist.
+	       */
+	      ResultSet rs = stmt.executeQuery("SELECT MAX(ID) AS maxid "
+	          + "FROM Organisationseinheit");
+	      
+	      if (rs.next()) {
+
+	        o.setID(rs.getInt("maxid") + 1);
+	        id=o.getID();
+	        stmt = con.createStatement();
+
+	        // Jetzt erst erfolgt die tatsächliche Einfügeoperation
+
+	        stmt.executeUpdate("INSERT INTO Organisationseinheit (ID, straße, hausnummer, plz, ort, Partnerprofil_ID) "
+	            + "VALUES ('" + o.getID() + "','" + o.getStraße() + "','"
+	            + o.getHausnummer() + "','" + o.getPlz() + "','" + o.getOrt() + "', " + o.getPartnerprofil_ID() + ")");
+	      }
+
+	    }
+	    catch (SQLException e) {
+	      e.printStackTrace();
+	    }
+
+	    return id;
+	  
+  }
+	
+	public Organisationseinheit update(Organisationseinheit o){
 		
-		 Connection con = DBConnection.connection();
-		 
-		 Vector<Projektmarktplatz> result = new Vector<Projektmarktplatz>();
-		 
-		  try {
+		Connection con = DBConnection.connection();
+
+		    try {
 		      Statement stmt = con.createStatement();
 
-		      ResultSet rs = stmt.executeQuery("SELECT ID, bez FROM Projektmarktplatz");
-		  
-		  while (rs.next()) {
-			  Projektmarktplatz p = new Projektmarktplatz();
-			  p.setID(rs.getInt("ID"));
-			  p.setBez(rs.getString("bez"));
-			  
-			  result.addElement(p);
-		  }
-		}
-		    catch (SQLException e2) {
-		        e2.printStackTrace();
-		      }
-		  return result;
-	}
+
+		      stmt.executeUpdate("UPDATE Organisationseinheit SET straße='"
+			          + o.getStraße() + "'," + "hausnummer='" + o.getHausnummer() + "'," + "plz=" + o.getPlz() + ","
+			          + "ort='" + o.getOrt() + "' WHERE ID="+o.getID());
+
+		      
+		    }
+		    catch (SQLException e) {
+		      e.printStackTrace();
+		    }
+
+		    return o;
+	  }
 	
 	
-	public Projektmarktplatz addMarktplatz(Projektmarktplatz pmp){
-		
-		Connection con = DBConnection.connection();
-		
-		try {
+	public void delete(Organisationseinheit o){
+		 Connection con = DBConnection.connection();
+
+		    try {
 		      Statement stmt = con.createStatement();
+
+		      stmt.executeUpdate("DELETE FROM Organisationseinheit" + " WHERE ID=" + o.getID());
 		      
-		      ResultSet rs = stmt.executeQuery("SELECT MAX(ID) AS maxid "
-		              + "FROM Projektmarktplatz ");
-		      
-		
-		      if(rs.next()){
-		    	  
-		    	  	pmp.setID(rs.getInt("maxid") + 1);
-		    	  
-		    	  	stmt = con.createStatement();
-		    	  	
-		    		stmt.executeUpdate("INSERT INTO Projektmarktplatz (ID , bez)" + "VALUES (" 
-		    		+ pmp.getID() + "," + "'" + pmp.getBez() + "'" +")");
-		    	  
-		      }
-		}
-		catch(SQLException e2){
-			e2.printStackTrace();
-		}
-		return pmp;
-		
-	}
-	
-	
-	public Projektmarktplatz updateMarktplatz(Projektmarktplatz p){
-		
-		Connection con = DBConnection.connection();
-		
-		try{
-			
-			Statement stmt = con.createStatement();
-			
-			stmt.executeUpdate(" UPDATE Projektmarktplatz " + "SET bez =\""
-								+ p.getBez() + "\" " + "WHERE ID= " + p.getID());
-		
-			}
-		catch (SQLException e2) {
-			e2.printStackTrace();
-		}
-			
-		return p;
-	}
-	public Projektmarktplatz deleteMarktplatz(Projektmarktplatz p){
-		
-		Connection con = DBConnection.connection();
-	
-		try{
-			
-			Statement stmt = con.createStatement();
-			
-			stmt.executeUpdate(" DELETE FROM Projektmarktplatz " +  "WHERE ID= " + p.getID());
-		
-			}
-		catch (SQLException e2) {
-			e2.printStackTrace();
-		}
-			
-		return p;
-	}
-	
-	
-	
+		    }
+		    catch (SQLException e) {
+		      e.printStackTrace();
+		    }
+	  }	
 	
 }
--- src/de/hdm/ITProjekt/shared/bo/Organisationseinheit.java
+++ src/de/hdm/ITProjekt/shared/bo/Organisationseinheit.java
@@ -6,34 +6,14 @@ public class Organisationseinheit extends BusinessObject{
 	
 	private static final long serialVersionUID = 1L;
 
-	private int ID = 0;	
-	private String vorname = "";
-	private String nachname = "";
 	private String straße = "";
 	private int hausnummer = 0;
 	private int plz = 0;
 	private String ort = "";
-	private int Partnerprofil_ID = 0;
+	
+	private Integer Partnerprofil_ID = 0;
 	
 	
-	public int getID() {
-		return ID;
-	}
-	public void setID(int iD) {
-		ID = iD;
-	}
-	public String getVorname() {
-		return vorname;
-	}
-	public void setVorname(String vorname) {
-		this.vorname = vorname;
-	}
-	public String getNachname() {
-		return nachname;
-	}
-	public void setNachname(String nachname) {
-		this.nachname = nachname;
-	}
 	public String getStraße() {
 		return straße;
 	}
@@ -57,20 +37,23 @@ public class Organisationseinheit extends BusinessObject{
 	}
 	public void setOrt(String ort) {
 		this.ort = ort;
-	}
-	public int getPartnerprofil_ID() {
+	}	
+	public Integer getPartnerprofil_ID() {
 		return Partnerprofil_ID;
 	}
-	public void setPartnerprofil_ID(int partnerprofil_ID) {
-		Partnerprofil_ID = partnerprofil_ID;
+	public void setPartnerprofil_ID(Integer partnerprofil_ID) {
+		if(partnerprofil_ID==0){
+			this.Partnerprofil_ID=null;
+		}else{
+			this.Partnerprofil_ID = partnerprofil_ID;	
+		}
 	}
 	
-	public String toString(){
-		return "\n" + "ID: " + ID + "\n" + "Vorname: " + vorname + "\n" + "Nachname: " 
-				+ vorname + "\n" + "Straße: " + straße + "\n" + "Hausnummer: " + hausnummer
-				+ "\n" + "Postleitzahl: " + plz + "\n" + "Wohnort: " + ort + "\n" 
-				+ "Diese Organisationseinheit gehört zu diesem Partnerprofil: " + Partnerprofil_ID;
-	}
+//	public String toString(){
+//		return "\n" + "ID: " + ID + "\n" + "Straße: " + straße + "\n" + "Hausnummer: " + hausnummer
+//				+ "\n" + "Postleitzahl: " + plz + "\n" + "Wohnort: " + ort + "\n" 
+//				+ "Diese Organisationseinheit gehört zu diesem Partnerprofil: " + Partnerprofil_ID;
+//	}
 	public boolean equals(Object o) {
 
 	    if (o != null && o instanceof Organisationseinheit) {
