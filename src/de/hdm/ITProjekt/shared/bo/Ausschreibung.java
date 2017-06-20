3f6fb3faf4dfd55c7c9e51f09584c425bb9b5a3e
MarcusReiss <mr154@hdm-stuttgart.de>
2017-06-20 16:59:26 +0200

    Dokumentation bearbeitet

--- src/de/hdm/ITProjekt/shared/bo/Ausschreibung.java
+++ src/de/hdm/ITProjekt/shared/bo/Ausschreibung.java
@@ -11,67 +11,181 @@ public class Ausschreibung extends BusinessObject {
 	private String bezeichnung = "";
 	private Date datum = null;
 	
-	//Realisierung der Fremdschlüssel Beziehungen
+	/*
+	 * Realisierung der Beziehungen zu Projekt mittels Fremdschlüssel
+	 */
 	
 	private int Projekt_ID = 0;
 	
+	/*
+	 * Realisierung der Beziehung zu einem Partnerprofil mittels Fremdschlüssel
+	 */
+	
 	private int Partnerprofil_ID = 0;
 	
+	/*
+	 * Realisierung der Beziehung zu Organisationseinheit mittels Fremdschlüssel
+	 */
+	
 	private int Orga_ID = 0;
+	
+	/*
+	 * 
+	 * @param selectedObject
+	 */
 	
 	
 	public Ausschreibung(Ausschreibung selectedObject) {
 		// TODO Auto-generated constructor stub
 	}
+	
+	/*
+	 * @param 
+	 */
+	
 	public Ausschreibung() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	/*
+	 * @return ID
+	 */
+	
 	public int getID() {
 		return ID;
 	}
+	
+	/*
+	 *  @param ID
+	 */
+	
 	public void setID(int iD) {
 		ID = iD;
 	}
+	
+	/*
+	 * 
+	 * @return ausschreibungstext
+	 */
+	
 	public String getAusschreibungstext() {
 		return ausschreibungstext;
 	}
+	
+	/*
+	 * 
+	 * @param ausschreibungstext
+	 */
+	
 	public void setAusschreibungstext(String ausschreibungstext) {
 		this.ausschreibungstext = ausschreibungstext;
 	}
+	/*
+	 * 
+	 * @return
+	 */
+	
 	public String getBezeichnung() {
 		return bezeichnung;
 	}
+	
+	/*
+	 * 
+	 * @param bezeichnung
+	 */
+	
 	public void setBezeichnung(String bezeichnung) {
 		this.bezeichnung = bezeichnung;
 	}
+	
+	/*
+	 * 
+	 * @return
+	 */
+	
 	public Date getDatum() {
 		return datum;
 	}
+	
+	/*
+	 * 
+	 * @param datum
+	 */
+	
 	public void setDatum(Date datum) {
 		this.datum = datum;
-	}	
+	}
+	
+	/*
+	 * 
+	 * @return
+	 */
+	
 	public int getProjekt_ID() {
 		return Projekt_ID;
 	}
+	
+	/*
+	 * 
+	 * @param projekt_ID
+	 */
+	
 	public void setProjekt_ID(int projekt_ID) {
 		Projekt_ID = projekt_ID;
 	}
+	
+	/*
+	 * 
+	 * @return
+	 */
+	
 	public int getPartnerprofil_ID() {
 		return Partnerprofil_ID;
 	}
+	
+	/*
+	 * 
+	 * @param partnerprofil_ID
+	 */
+	
 	public void setPartnerprofil_ID(int partnerprofil_ID) {
 		Partnerprofil_ID = partnerprofil_ID;
 	}
+	
+	/*
+	 * 
+	 * @return
+	 */
+	
 	public int getOrga_ID() {
 		return Orga_ID;
 	}
+	
+	/*
+	 * 
+	 * @param orga_ID
+	 */
+	
 	public void setOrga_ID(int orga_ID) {
 		Orga_ID = orga_ID;
 	}
+	
+	/*
+	 * zum Testen der Mapper in der Konsole
+	 * @return "\n" + "ID: " + ID + "\n" + "Ausschreibungstext: " + ausschreibungstext + "\n"
+	 * 			+ "Bezeichnung: " + bezeichnung + "\n" + "Datum: " + datum;
+	 */
+	
 	public String toString(){
 		return "\n" + "ID: " + ID + "\n" + "Ausschreibungstext: " + ausschreibungstext + "\n"
 				+ "Bezeichnung: " + bezeichnung + "\n" + "Datum: " + datum;
 	}
+	
+	/*
+	 * @param
+	 * @return super.equals(c)
+	 * @return false
+	 */
 	
 	public boolean equals(Object o) {
 
--- src/de/hdm/ITProjekt/shared/bo/Bewertung.java
+++ src/de/hdm/ITProjekt/shared/bo/Bewertung.java
@@ -5,83 +5,138 @@ public class Bewertung extends BusinessObject {
 	
 	private static final long serialVersionUID = 1L;
 	
+	/*
+	 * eindeutige Identifikationsnummer einer Instanz der Klasse
+	 */
+	
 	private int ID = 0;
+	
+	/*
+	 * Text der Stellungsnahme 
+	 */
+	
 	private String stellungnahme = "";
+	
+	/*
+	 * Score der Bewertung
+	 */
+	
 	private double bewertung = 0.0;
-	//private Date erstellungsdatum;
+	
+	/*
+	 * Erstelungsdatum der Bewertung
+	 */
+	
+	private Date erstellungsdatum;
+	
+	/*
+	 * Realisieren der Beziehung zu einer Beteiligung mittels Fremdschlüssel
+	 */
+	
 	private int Beteiligungs_ID = 0;
+	
+	/*
+	 * Realisieren der Beziehung zu einer Bewerbung mittels Fremdschlüssel
+	 */
+	
 	private int Bewerbungs_ID = 0;
 	
+	/*
+	 * Auslesen der ID
+	 * @return ID
+	 */
 	
 	public int getID() {
 		return ID;
 	}
-
-
-
+	 
+	/*
+	 * Setzen der ID
+	 * @param ID
+	 */
 
 	public void setID(int iD) {
 		ID = iD;
 	}
-
-
-
-
+	
+	/*
+	 * Auslesen der Stellungnahme
+	 * @return stellungnahme
+	 */
+	
 	public String getStellungnahme() {
 		return stellungnahme;
 	}
-
-
-
-
+	
+	/*
+	 * Setzen des Textes zu Stellungnahme
+	 * @param stellungnahme
+	 */
+	
 	public void setStellungnahme(String stellungnahme) {
 		this.stellungnahme = stellungnahme;
 	}
 
-
-
+	/*
+	 * Auslesen der Bewertung
+	 * @return Bewertung
+	 */
 
 	public double getBewertung() {
 		return bewertung;
 	}
-
-
-
+	
+	/*
+	 * Setzen der Bewertung
+	 * @param Bewertung
+	 */
 
 	public void setBewertung(double bewertung) {
 		this.bewertung = bewertung;
 	}
 
-
-
-
+	/*
+	 * Auslesen des Fremdschlüssels der Beteiligungs ID
+	 * @param Beteiligungs_ID
+	 */
+	
 	public int getBeteiligungs_ID() {
 		return Beteiligungs_ID;
 	}
-
-
-
+	
+	/*
+	 * Setzen des Fremdschlüssels Beteiligungs_ID
+	 * @param Beteiligungs_ID
+	 */
 
 	public void setBeteiligungs_ID(int beteiligungs_ID) {
 		Beteiligungs_ID = beteiligungs_ID;
 	}
-
-
-
-
+	
+	/*
+	 * Auslesen des Fremdschlüssels Bewerbungs ID
+	 * @return Bewerbungs_ID
+	 */
+	
 	public int getBewerbungs_ID() {
 		return Bewerbungs_ID;
 	}
-
-
-
+	
+	/*
+	 * Setzen des Fremdschlüssels Bewerbungs ID
+	 * @param Bewerbungs_ID
+	 */
 
 	public void setBewerbungs_ID(int bewerbungs_ID) {
 		Bewerbungs_ID = bewerbungs_ID;
 	}
-
-
-
+	
+	/*
+	 * zum Testen der Mapper in der Konsole
+	 * @return "\n" + "ID: " + ID + "\n" + "Stellungnahme: " + stellungnahme + "\n"
+	 * 	+ "Beurteilung: " + bewertung + "\n" + "Diese Beteiligung gehört zu folgender Bewerbung: " + Bewerbungs_ID
+	 * 		 + "\n";
+	 */
 
 	public String toString(){
 		return "\n" + "ID: " + ID + "\n" + "Stellungnahme: " + stellungnahme + "\n"
