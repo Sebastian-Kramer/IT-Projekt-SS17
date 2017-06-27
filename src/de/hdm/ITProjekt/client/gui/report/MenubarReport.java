711ccaa7d969e03650014bb718717d58b39822e8
GiuseppeGGalati <gg025@hdm-stuttgart.de>
2017-06-27 12:46:16 +0200

    Clickhandler hinzugefügt

--- src/de/hdm/ITProjekt/client/gui/report/MenubarReport.java
+++ src/de/hdm/ITProjekt/client/gui/report/MenubarReport.java
@@ -8,6 +8,7 @@ import com.google.gwt.user.client.ui.StackPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 import de.hdm.ITProjekt.client.ClientsideSettings;
+import de.hdm.ITProjekt.client.ReportShowcase;
 import de.hdm.ITProjekt.client.Showcase;
 import de.hdm.ITProjekt.client.gui.IdentitySelection;
 import de.hdm.ITProjekt.client.gui.MeinProfilAnzeigen;
@@ -17,6 +18,7 @@ import de.hdm.ITProjekt.client.gui.ProjektmarktplatzSeite;
 import de.hdm.ITProjekt.client.gui.StellenausschreibungenSeite;
 import de.hdm.ITProjekt.shared.AdministrationProjektmarktplatzAsync;
 import de.hdm.ITProjekt.shared.bo.Person;
+import de.hdm.ITProjekt.shared.report.AllBewerbungenToOneAusschreibungReport;
 
 public class MenubarReport extends StackPanel{
 	// Clickhandler auf "null" setzen
@@ -79,8 +81,10 @@ public class MenubarReport extends StackPanel{
 		
 			alleBewerbungenByPerson.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
-				
-				
+			ReportShowcase reportShowcase = new AllBewerbungenByPersonShowcase();
+			RootPanel.get("DetailsReport").clear();
+			RootPanel.get("DetailsReport").add(reportShowcase);
+
 				currentClickHandler=this;
 				currentClickEvent=event;
 				
@@ -89,7 +93,9 @@ public class MenubarReport extends StackPanel{
 			
 		alleAusschreibungen.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
-				
+				ReportShowcase reportShowcase = new AllAusschreibungenShowcase();
+				RootPanel.get("DetailsReport").clear();
+				RootPanel.get("DetailsReport").add(reportShowcase);
 				currentClickHandler=this;
 				currentClickEvent=event;
 				
@@ -98,7 +104,9 @@ public class MenubarReport extends StackPanel{
 		
 		alleAusschreibungenzupartnerprofil.addClickHandler(new ClickHandler(){
 			public void onClick(ClickEvent event){
-				
+				ReportShowcase reportShowcase = new AllAusschreibungenByPartnerprofilShowcase();
+				RootPanel.get("DetailsReport").clear();
+				RootPanel.get("DetailsReport").add(reportShowcase);
 				currentClickHandler=this;
 				currentClickEvent=event;
 			}
@@ -114,12 +122,18 @@ public class MenubarReport extends StackPanel{
 		
 			faninfanoutanalyse.addClickHandler(new ClickHandler(){
 			public void onClick(ClickEvent event){
+				ReportShowcase reportShowcase = new FanInFanOutShowcase();
+				RootPanel.get("DetailsReport").clear();
+				RootPanel.get("DetailsReport").add(reportShowcase);
 				currentClickHandler=this;
 				currentClickEvent=event;
 				
 					
 			projektverflechtungen.addClickHandler(new ClickHandler(){
 			public void onClick(ClickEvent event){
+				ReportShowcase reportShowcase = new ProjektverflechtungenShowcase();
+				RootPanel.get("DetailsReport").clear();
+				RootPanel.get("DetailsReport").add(reportShowcase);
 				currentClickHandler=this;
 				currentClickEvent=event;
 		
